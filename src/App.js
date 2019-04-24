be4310bed311f690723633755c0178021f2daddc
roycuenca <roycuenca2013@gmail.com>
2019-04-24 16:49:32 -0300

    react project-citas

--- src/App.js
+++ src/App.js
@@ -1,28 +1,74 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Header from './components/Header';
+import AgregarCita from "./components/AgregarCita";
+import ListaCitas from "./components/ListaCitas";
 
-class App extends Component {
+class Citas extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+        citas: []
+      };
+
+      this.crearCita = this.crearCita.bind(this);
+  }
+
+  componentDidMount(){
+    const citasLS = localStorage.getItem('citas');
+    if(citasLS) {
+      this.setState({
+        citas: JSON.parse(citasLS)
+      })
+    }
+  }
+
+  componentDidUpdate(){
+    localStorage.setItem(
+      'citas',
+      JSON.stringify(this.state.citas)
+    )
+  }
+
+  crearCita(nuevaCita) {
+    const citas = [...this.state.citas, nuevaCita];
+
+    this.setState({
+      citas
+    });
+  }
+
+  borrarCita = id => {
+    const citasActuales =[...this.state.citas];
+    const citas = citasActuales.filter(cita => cita.id !== id);
+    this.setState({
+      citas
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <Header
+          titulo={'Administrador de pacientes de veterinaria'}  
+        />
+
+        <div className="row">
+          <div className="col-md-6">
+            <AgregarCita
+               crearCita={this.crearCita} 
+            />
+          </div>
+          <div className="col-md-6">
+            <ListaCitas 
+              citas={this.state.citas}
+              borrarCita={this.borrarCita}
+            />
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default Citas;
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const Header = props => {
+    return(
+        <header>
+            <h1 
+                className="text-center">
+                {props.titulo}
+            </h1>
+        </header>
+    )
+   
+}
+Header.propType = {
+    titulo: PropTypes.string.isRequired
+}
+
+export default Header;
